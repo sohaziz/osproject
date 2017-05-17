ac94068a09eef0ac1a1e51c18724944d26e241da
sohaziz <sohaziz22@gmail.com>
2017-05-17 14:40:02 +0300

    Update MainScreen.java
    add function to top selling

--- src/simple_ice_cream/MainScreen.java
+++ src/simple_ice_cream/MainScreen.java
@@ -33,10 +33,26 @@ public class MainScreen extends javax.swing.JFrame {
         this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
         this.FillTopSelling();
     }
-    private void FillTopSelling()
+    private void FillTopSelling(){
+         ResultSet data =   clsOrderItem.TopSellingItems();
+         if (data ==null)
          {
              return;
          }
+         try{
+             data.next();
+         lblTopItemName.setText(data.getString("type"));
+         lblSellingCount.setText(data.getString("sellingcount"));
+         }
+         catch (Exception ex)
+         {
+             //JOptionPane.showMessageDialog(null, ex.getMessage());
+             System.err.println(ex.getMessage());
+             lblTopItemName.setText("");
+             lblSellingCount.setText("");
+         }
+         
+    }
        
     public MainScreen(int Quantity,double total,IceCreamSelling screen) {
         initComponents();
